4b74ac8a01876bff3730e935dd5cad1065fac49e
sjgeyer <sjgeyer@comcast.net>
2018-02-26 15:14:04 -0800

    combine js for game.html

--- js/random.js
+++ js/random.js
@@ -1,6 +1,6 @@
 var randoms = [];
 function addRandom(){
-    randoms += Math.floor((Math.random() * 4)+1);
-    console.log(randoms);
+  randoms += Math.floor((Math.random() * 4)+1);
+  console.log(randoms);
 };
 
--- js/userturn.js
+++ js/userturn.js
@@ -1,33 +1,33 @@
 var myTurn = document.getElementById('myTurn')
 
 function userTurn(){
-    myTurn.style.display === "none";
-    yourTurn.style.display === "block";
+  myTurn.style.display === "none";
+  yourTurn.style.display === "block";
 
-    var keyPresses = [];
-window.addEventListener("keydown", function (event) {
+  var keyPresses = [];
+  window.addEventListener("keydown", function (event) {
     if (event.defaultPrevented) {
       return; // Do nothing if the event was already processed
     }
   
     switch (event.key) {
-      case "ArrowDown":
-        keyPresses +=4;
-        var newArrow = document.createElement('img');
-        newArrow.src = 'images/arrow-d.png';
-        arrowImages.appendChild('newArrow');
-        break;
-      case "ArrowUp":
-        keyPresses +=2;
-        break;
-      case "ArrowLeft":
-        keyPresses +=1;
-        break;
-      case "ArrowRight":
-        keyPresses +=3;
-        break;
-      default:
-        return; // Quit when this doesn't handle the key event.
+    case "ArrowDown":
+      keyPresses +=4;
+      var newArrow = document.createElement('img');
+      newArrow.src = 'images/arrow-d.png';
+      arrowImages.appendChild('newArrow');
+      break;
+    case "ArrowUp":
+      keyPresses +=2;
+      break;
+    case "ArrowLeft":
+      keyPresses +=1;
+      break;
+    case "ArrowRight":
+      keyPresses +=3;
+      break;
+    default:
+      return; // Quit when this doesn't handle the key event.
     }
     // Cancel the default action to avoid it being handled twice
     event.preventDefault();
