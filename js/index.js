35ef00421f0173343219c6282e853f292b627c3e
sjgeyer <sjgeyer@comcast.net>
2018-02-26 15:23:47 -0800

    clean up index.html after merge conflict

--- js/index.js
+++ js/index.js
@@ -1,19 +1,15 @@
 'use strict';
 
+var userForm = document.getElementById('userForm');
 
-var userForm = document.getElementById("userForm");
-
-userForm.addEventListener("submit", function(event) {
+userForm.addEventListener('submit', function(event) {
   event.preventDefault();
   if(event.target.input.value.length < 3){
-    alert("Name is too short!")
+    alert('Name is too short!');
     event.target.input.value = null;
   }
   var userName = event.target.input.value;
-  localStorage.setItem("user",JSON.stringify(userName));
-  document.getElementById("you").innerHTML = JSON.parse(localStorage.getItem("user", JSON.stringify(userName)));
-  window.location.assign("game.html")
-
-
-
+  localStorage.setItem('user',JSON.stringify(userName));
+  document.getElementById('you').innerHTML = JSON.parse(localStorage.getItem('user', JSON.stringify(userName)));
+  window.location.assign('game.html');
 });
