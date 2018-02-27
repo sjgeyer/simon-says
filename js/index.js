011cb5a70e18bb91a1326a464099e7271c234790
Matthew Gordon <matthewgordon89@gmail.com>
2018-02-26 16:51:43 -0800

    userName + can you keep up

--- js/index.js
+++ js/index.js
@@ -9,7 +9,8 @@ userForm.addEventListener('submit', function(event) {
     event.target.input.value = null;
   }
   var userName = event.target.input.value;
-  localStorage.setItem('user',JSON.stringify(userName));
-  document.getElementById('you').innerHTML = JSON.parse(localStorage.getItem('user', JSON.stringify(userName)));
+  localStorage.setItem('userName',JSON.stringify(userName));
+  console.log(localStorage.userName);
+  // document.getElementById('you').innerHTML = JSON.parse(localStorage.getItem('userName', JSON.stringify(userName)));
   window.location.assign('game.html');
 });
