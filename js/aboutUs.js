38144c0a3e9346004928f15a558d12fc08de5cde
sjgeyer <sjgeyer@comcast.net>
2018-02-28 10:46:26 -0800

    about us animation - ghosts jump back into place with different arrow key

--- js/aboutUs.js
+++ js/aboutUs.js
@@ -23,37 +23,51 @@ window.addEventListener('keydown', function (event) {
   event.preventDefault();
 }, true);
 
-document.getElementById('info').hidden = true;
 
+document.getElementById('info').hidden = true;
+var sarahImg = document.getElementById('imgSarah');
+var collinImg = document.getElementById('imgCollin');
+var mattImg = document.getElementById('imgMatt');
+var jamesImg = document.getElementById('imgJames');
+var images = [sarahImg, collinImg, mattImg, jamesImg];
+
+function clear(id) {
+  for (var i = 0; i < images.length; i++) {
+    if(images[i].classList && images[i] !== id) {
+      images[i].classList.remove('focusL');
+      images[i].classList.remove('focusR');
+    }
+  }
+}
 function makeCollin(){
   var collinInfo = 'This is where Collin\'s info will go';
   document.getElementById('info').innerHTML = collinInfo;
-  document.getElementById('info').hidden = false;
   document.getElementById('info').style.borderColor = '#0000ff';
-  var collinImg = document.getElementById('imgCollin');
   collinImg.classList.toggle('focusR');
+  clear(collinImg);
+  document.getElementById('info').hidden = false;
 }
 function makeSarah(){
   var sarahInfo = 'Sarah is an environmental activist	turned coding enthusiast. She enjoys eating burritos, watching true crime documentaries, and falling asleep on other people\'s couches.';
   document.getElementById('info').innerHTML = sarahInfo;
-  document.getElementById('info').hidden = false;
   document.getElementById('info').style.borderColor = '#ff0000';
-  var sarahImg = document.getElementById('imgSarah');
   sarahImg.classList.toggle('focusL');
+  clear(sarahImg);
+  document.getElementById('info').hidden = false;
 }
 function makeMatt(){
   var mattInfo = 'This is where Matt\'s info will go';
   document.getElementById('info').innerHTML = mattInfo;
-  document.getElementById('info').hidden = false;
   document.getElementById('info').style.borderColor = '#fff000';
-  var mattImg = document.getElementById('imgMatt');
   mattImg.classList.toggle('focusL');
+  clear(mattImg);
+  document.getElementById('info').hidden = false;
 }
-function makeJames(){ 
+function makeJames(){
   var jamesInfo = 'When James isn\'t breaking code, he nerds out on bicycles, camping, and incomplete lists.';
   document.getElementById('info').innerHTML = jamesInfo;
-  document.getElementById('info').hidden = false;
   document.getElementById('info').style.borderColor = '#00ff00';
-  var jamesImg = document.getElementById('imgJames');
   jamesImg.classList.toggle('focusR');
+  clear(jamesImg);
+  document.getElementById('info').hidden = false;
 }
