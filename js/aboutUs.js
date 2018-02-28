b4dd42ae1515b9d75189a7e6448c17751338916b
sjgeyer <sjgeyer@comcast.net>
2018-02-28 11:18:51 -0800

    more edits to about us animation

--- js/aboutUs.js
+++ js/aboutUs.js
@@ -30,12 +30,16 @@ var mattImg = document.getElementById('imgMatt');
 var jamesImg = document.getElementById('imgJames');
 var images = [sarahImg, collinImg, mattImg, jamesImg];
 
-function clear(id) {
+function clear(id, className) {
+  document.getElementById('info').hidden = true;
   for (var i = 0; i < images.length; i++) {
     if(images[i].classList && images[i] !== id) {
       images[i].classList.remove('focusL');
       images[i].classList.remove('focusR');
     }
+  }
+  if(id.classList.value === className) {
+    document.getElementById('info').hidden = false;
   }
 }
 function makeCollin(){
@@ -43,30 +47,26 @@ function makeCollin(){
   document.getElementById('info').innerHTML = collinInfo;
   document.getElementById('info').style.borderColor = '#0000ff';
   collinImg.classList.toggle('focusR');
-  clear(collinImg);
-  document.getElementById('info').hidden = false;
+  clear(collinImg, 'focusR');
 }
 function makeSarah(){
   var sarahInfo = 'Sarah is an environmental activist	turned coding enthusiast. She enjoys eating burritos, watching true crime documentaries, and falling asleep on other people\'s couches.';
   document.getElementById('info').innerHTML = sarahInfo;
   document.getElementById('info').style.borderColor = '#ff0000';
   sarahImg.classList.toggle('focusL');
-  clear(sarahImg);
-  document.getElementById('info').hidden = false;
+  clear(sarahImg, 'focusL');
 }
 function makeMatt(){
   var mattInfo = 'This is where Matt\'s info will go';
   document.getElementById('info').innerHTML = mattInfo;
   document.getElementById('info').style.borderColor = '#fff000';
   mattImg.classList.toggle('focusL');
-  clear(mattImg);
-  document.getElementById('info').hidden = false;
+  clear(mattImg, 'focusL');
 }
 function makeJames(){
   var jamesInfo = 'When James isn\'t breaking code, he nerds out on bicycles, camping, and incomplete lists.';
   document.getElementById('info').innerHTML = jamesInfo;
   document.getElementById('info').style.borderColor = '#00ff00';
   jamesImg.classList.toggle('focusR');
-  clear(jamesImg);
-  document.getElementById('info').hidden = false;
+  clear(jamesImg, 'focusR');
 }
