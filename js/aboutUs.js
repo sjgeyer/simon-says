f63c7aeee9252e6676c872a83915e04755d920d8
James McDaniel <icathaid@gmail.com>
2018-02-28 12:07:40 -0800

    finished js logic

--- js/aboutUs.js
+++ js/aboutUs.js
@@ -26,23 +26,67 @@ function makeCollin(){
   var collinInfo = 'This is where Colin\'s info will go';
   document.getElementById("info").innerHTML = collinInfo;
   var collinImg = document.getElementById("imgCollin");
-  collinImg.classList.toggle("focusR");
+  collinImg.classList.toggle("focusD");
+  if(imgSarah.classList.length === 1){
+    ghostSarah();
+  } else if (imgMatt.classList.length === 1){
+    ghostMatt();
+  } else if (imgJames.classList.length === 1){
+    ghostJames();
+  };
 }
 function makeSarah(){
   var sarahInfo = 'This is where Sarah\'s info will go';
   document.getElementById("info").innerHTML = sarahInfo;
   var sarahImg = document.getElementById("imgSarah");
   sarahImg.classList.toggle("focusL");
+  if (imgJames.classList.length === 1){
+    ghostJames();
+  } else if(imgCollin.classList.length === 1){
+    ghostCollin();
+  } else if(imgMatt.classList.length === 1){
+    ghostMatt();
+  };
 }
 function makeMatt(){
   var mattInfo = 'This is where Matt\'s info will go';
   document.getElementById("info").innerHTML = mattInfo;
-  var mattImg = document.getElementById("imgMatt");
-  mattImg.classList.toggle("focusL");
+  var imgMatt = document.getElementById("imgMatt");
+  imgMatt.classList.toggle("focusU");
+  if (imgJames.classList.length === 1){
+    ghostJames();
+  } else if (imgCollin.classList.length === 1){
+    ghostCollin();
+  } else if (imgSarah.classList.length === 1){
+    ghostSarah();
+  };
 }
 function makeJames(){ 
   var jamesInfo = 'When James isn\'t breaking code, he nerds out on bicycles, camping, and incomplete lists.';
   document.getElementById("info").innerHTML = jamesInfo;
-  var jamesImg = document.getElementById("imgJames");
-  jamesImg.classList.toggle("focusR");
+  var imgJames = document.getElementById("imgJames");
+  imgJames.classList.toggle("focusR");
+  if (imgMatt.classList.length === 1){
+    ghostMatt();
+  } else if (imgSarah.classList.length === 1){
+    ghostSarah();
+  } else if (imgCollin.classList.length === 1){
+    ghostCollin();
+  };
 }
+function ghostMatt(){
+  var imgMatt = document.getElementById("imgMatt");
+  imgMatt.classList.toggle("focusU");
+};
+function ghostSarah(){
+  var imgSarah = document.getElementById("imgSarah");
+  imgSarah.classList.toggle("focusL");
+};
+function ghostJames(){
+  var imgJames = document.getElementById("imgJames");
+  imgJames.classList.toggle("focusR");
+};
+function ghostCollin(){
+  var imgCollin = document.getElementById("imgCollin");
+  imgCollin.classList.toggle("focusD");
+};
