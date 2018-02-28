34fdd289590a7ca39a9b06f48446a7a4a9f5e22b
James McDaniel <icathaid@gmail.com>
2018-02-27 18:31:53 -0800

    about me basic js done

--- js/aboutUs.js
+++ js/aboutUs.js
@@ -6,19 +6,15 @@ window.addEventListener('keydown', function (event) {
   }
   switch (event.key) {
   case 'ArrowDown':
-    console.log('down arrow');
     makeCollin();
     break;
   case 'ArrowUp':
-    console.log('up arrow');
     makeMatt();
     break;
   case 'ArrowLeft':
-    console.log('left arrow');
     makeSarah();
     break;
   case 'ArrowRight':
-    console.log('right arrow');
     makeJames();
     break;
   default:
@@ -28,38 +24,18 @@ window.addEventListener('keydown', function (event) {
 }, true);
 
 function makeCollin(){
-  console.log('bind collin\'s info to the dom and append it?');
-  console.log('put rest of function here manipulating images');
-  var collin = document.createElement('div');
-  //create blank div, use this with innerhtml to rewrite <p> content within div
-  collin.innerHTML = '<p>Collin\'s info will go here.</p>';
-  var collinP = document.createTextNode("Collin's info will go here");
-  collin.appendChild(collinP);
-  document.body.appendChild(collinP);
-  //need to bind this to an element with an ID, maybe a <div> under after the <img> but in the same container?
+  var collinInfo = 'This is where Colin\'s info will go';
+  document.getElementById("info").innerHTML = collinInfo;
 }
 function makeSarah(){
-  console.log('bind sarah\'s info to the dom and append it?');
-  console.log('put rest of function here manipulating images');
-  var sarah = document.createElement("div");
-  sarah.innerHTML = '<p>Sarah\'s info will go here.<p>';
-  var sarahP = document.createTextNode("Sara's info will go here");
-  sarah.appendChild(sarahP);
-  document.body.appendChild(sarahP);
+  var sarahInfo = 'This is where Sarah\'s info will go';
+  document.getElementById("info").innerHTML = sarahInfo;
 }
 function makeMatt(){
-  console.log('bind matt\'s info to the dom and append it?');
-  console.log('put rest of function here manipulating images');
-  var matt = document.createElement("p");
-  var mattP = document.createTextNode("Matt's info will go here");
-  matt.appendChild(mattP);
-  document.body.appendChild(mattP);
+  var mattInfo = 'This is where Matt\'s info will go';
+  document.getElementById("info").innerHTML = mattInfo;
 }
 function makeJames(){
-  console.log('bind james\' info to the dom and append it?');
-  console.log('put rest of function here manipulating images');
-  var james = document.createElement("p");
-  var jamesP = document.createTextNode("When James isn't breaking code, he nerds out on bicycles, camping, and incomplete lists.");
-  james.appendChild(jamesP);
-  document.body.appendChild(jamesP);
+  var jamesInfo = 'When James isn\'t breaking code, he nerds out on bicycles, camping, and incomplete lists.';
+  document.getElementById("info").innerHTML = jamesInfo;
 }
